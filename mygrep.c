0142fd699d6a4a4dc10770f1f8c7471a1e9c7233
Dorjee Dhondup <dhondo01@luther.edu>
2016-03-11 14:52:14 -0600

    Add documentation for mygrep.c

---
+++ mygrep.c
@@ -0,0 +1,7 @@
+/*
+File: mygrep.c
+Author: Dorjee Dhondup
+Date: March 11, 2016
+Description: This C program takes just one string and a list of files from the command-line
+             and prints the file name followed by a colon, followed by the line where the pattern occurs.
+*/
