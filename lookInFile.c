04fe807c67b1f18c33f1936100bcc788b028f2ff
Dorjee Dhondup <dhondo01@luther.edu>
2016-03-11 15:03:26 -0600

    Add lookInFile.c

---
+++ lookInFile.c
@@ -0,0 +1,14 @@
+/*
+File: lookInFile.c
+Author: Dorjee Dhondup
+Date: March 11, 2016
+Description: This C program is a separate source file with the implementation of lookInFile function inside it.
+             The function should print each line that matches to standard output with the filename, a colon,
+             and the matching line. The function does not need to return anything.
+*/
+
+
+int lookInFile(/* arguments */) {
+  /* code */
+  return 0;
+}
