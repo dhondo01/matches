6a36f3935a474f76a84b40b81534130359d8ae94
Dorjee Dhondup <dhondo01@luther.edu>
2016-03-11 14:49:01 -0600

    Add matches.sh

---
+++ matches.sh
@@ -0,0 +1,5 @@
+# File: matches.sh
+# Author: Dorjee Dhondup
+# Date: March 11, 2016
+# Description: This bash script should invoke mygrep program and counts
+#              the number of lines in a file that matched the input string.
