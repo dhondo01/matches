51d639dabd6e18040f16db33b3781e1c280c74a3
Dorjee Dhondup <dhondo01@luther.edu>
2016-03-12 13:42:09 -0600

    Add comments

--- mygrep.c
+++ mygrep.c
@@ -6,5 +6,12 @@ Description: This C program takes just one string and a list of files from the c
              and prints the file name followed by a colon, followed by the line where the pattern occurs.
 */
 
-#include <stdio.h>
+#include <stdio.h> /* This header file defines input/output routines */
+#include <string.h> /* cstring library: collection of fuctions for string manipulation */
 #include "lookInFile.h"
+
+int main(int argc, char const *argv[]) { /* main is the entry point of this program */
+  /* code */
+  printf("%s\n", Test );
+  return 0;
+}
