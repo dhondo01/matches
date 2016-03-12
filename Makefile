72a7638bc7f8e02554727470b44baaae43512bc6
Dorjee Dhondup <dhondo01@luther.edu>
2016-03-12 13:51:08 -0600

    Add annotations

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 # File: Makefile
 # Author: Dorjee Dhondup
 # Date: March 11, 2016
-# Description: This is the Makefile for the project. Makefile is a way to organize code comilation.
+# Description: This is the Makefile for the project. Makefile is a way to organize code compilation.
--- mygrep.c
+++ mygrep.c
@@ -6,12 +6,18 @@ Description: This C program takes just one string and a list of files from the c
              and prints the file name followed by a colon, followed by the line where the pattern occurs.
 */
 
+
+
 #include <stdio.h> /* This header file defines input/output routines */
 #include <string.h> /* cstring library: collection of fuctions for string manipulation */
 #include "lookInFile.h"
 
 int main(int argc, char const *argv[]) { /* main is the entry point of this program */
-  /* code */
-  printf("%s\n", Test );
+  /* strings are array of characters
+   * terminated by the NULL character
+   * which is different from '0' */
+
+  printf("%s\n", argc );
+
   return 0;
 }
