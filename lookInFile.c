4d44601caa8fe17f6343e9292b045371c0e02d08
Dorjee Dhondup <dhondo01@luther.edu>
2016-03-15 13:19:40 -0500

    Add basic function set up

--- lookInFile.c
+++ lookInFile.c
@@ -7,9 +7,28 @@ Description: This C program is a separate source file with the implementation of
              and the matching line. The function does not need to return anything.
 */
 
+#include <stdio.h>
 #include "lookInFile.h"
 
-int lookInFile(/* arguments */) {
-  /* code */
-  return 0;
+
+/* two arguments: filename, pattern */
+void lookInFile(char* filename, char* pattern) {
+
+	printf("In lookInFile function \n");
+
+	// FILE* file = fopen(filename, "r");
+
+	// int lines = 0;
+	// int numChars = 0;
+	// char line[maxLineLength];
+
+	// int done = readLine(file,line);
+
+	// while (done == 0) {
+	// 	done = readLine(file,line);
+	// 	lines++;
+	// 	numChars = numChars + strlen(line);
+	// }
+
+	// printf("There were %d characters and %d lines in file %s.\n",numChars,lines,filename);
 }
